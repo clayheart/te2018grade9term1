b42ac8da6110f918fa1f62aefbdcc4aadbbc5ab4
clay <cmheart2004@gmail.com>
2018-10-13 15:41:19 -0400

    first session

--- data.js
+++ data.js
@@ -1,5 +1,19 @@
 // Example
 let message = "SerpentxGaming!";
 console.log(message);
+ let youtube = 24;
+ console.log(youtube);
+let fortnite = 8;
+console.log(fortnite);
+let callofduty = 32;
+
+let clay = "juh";
+console.log (clay);
+
+5*6 == 30;
+console.log(5*6 == 30);
+4*3 == 12;
+console.log(4*3 == 12);
 
 // Type your code below!
+
