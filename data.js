5e83421a1c9d951ccb7fe52e4397e57fd5871800
Deziah <deziahgayle.04@gmail.com>
2018-10-13 15:43:35 -0400

    data.js (OCT. 13, 2018)

--- data.js
+++ data.js
@@ -3,3 +3,30 @@ let message = "Hello Deziah .... ";
 console.log(message);
 
 // Type your code below!
+
+let doesLikeBacon = true; 
+console.log(doesLikeBacon);
+
+//my work
+
+let number1 = 1; let number2 = 2; let number3 = 3
+console.log(number1, number2, number3);
+
+let message1 = "A: Hello. How are you?";
+
+let message2 = "B: I'm good. Thank you. What about you?";
+
+let message3 = "A: I'm good too. Thank you for asking.";
+console.log(message1, message2, message3);
+
+let ARworld = true;
+
+let message4 = "and";
+
+let REALworld = false;
+
+console.log(ARworld); console.log(message4); console.log(REALworld)
+
+let name; 
+console.log(name);
+
